f58078bb182ef791598b503c2e08a834bd3471c4
Vandevelde <raoul.vandevelde@gmail.com>
2023-11-30 14:03:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number inbetween 0 and 100: ");
+                
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high! Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low! Guess higher...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You are correct!");
+                    win = true;
+                }
+            }
+            while (win == false);
+        }
+    }
+}
